3d7bf3a59d96b0791efb32e80b9f9a09882edaf7
likehc <463259624@qq.com>
2018-08-02 03:40:02 +0800

    添加background.js 用于网络请求阻断

---
+++ js/BaseJs/background.js
@@ -0,0 +1,22 @@
+// https://rpic.douyucdn.cn/asrpic/*.jpg // 房间外显图片一定别阻止
+var blockUrls =[
+	"https://sta-op.douyucdn.cn/nggsys/*.jpg",	// 视频框内游戏推广
+	"https://sta-op.douyucdn.cn/nggsys/*.png",	// 视频框内游戏推广
+	"https://shark.douyucdn.cn/app/douyu/res/page/*.gif",	//源图片出错预备图
+	"https://cs-op.douyucdn.cn/nggsys/*.jpg",	//其它游戏推广
+	"https://cs-op.douyucdn.cn/nggsys/*.png",	//其它游戏推广	
+	"https://sta-op.douyucdn.cn/vod-cover/*.jpg",	//视频内的推荐图片
+	"https://sta-op.douyucdn.cn/vod-cover/*.png",	//视频内的推荐图片
+	"http://image.wan.douyu.com/upload/*.png",	//个人说明里的游戏推广
+	"https://shark.douyucdn.cn/app/douyu/res/com/*.jpg?*",	//斗鱼公会
+	"https://shark.douyucdn.cn//app/douyu/res/page/room-normal/clientdown/*.png?*",	//客户端下载页图片
+];
+var callback =function(details){
+	if (blockUrls.length<=0 || blockUrls[0] == "" || blockUrls[0] == null) {
+		return {cancel: false};
+	}
+	return {cancel: true};
+};
+var filter = {urls:blockUrls};
+var opt_extraInfoSpec = ["blocking"];
+chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
